31552fc41c862edf3f3ee4a665295f82adcee1bc
Albertojoel <68121487+Albertojoel@users.noreply.github.com>
2022-10-05 11:04:44 -0500

    Añandiendo logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
   import './App.css';
+  import freeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png';
 
 function App() {
   return (
     <div className="App">
-      Hola
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        src={freeCodeCampLogo}
+        className="freecodecamp-logo"
+        alt='Logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
